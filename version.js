a301468a141a4a480e9d7033ddcb3495b966f711
Raman Marozau <engineer.morozov@gmail.com>
2018-12-15 03:14:09 +0300

    Function: get Object value by path or default value Add project configuration property: pr.target Add verification of correct git target branch

--- version.js
+++ version.js
@@ -48,34 +48,43 @@ if (!currentVersion) {
   stop([`${ANSI_FG_RED}%s${ANSI_FG_NC}`, `${config.common.messages.incorrectVersionBranchNameLength} ${config.git.limits.branchMaxLength} characters. ${HELP_MESSAGE}`]);
 }
 
-exec('git status --porcelain', execAction((changes) => {
-  if (changes) {
-    stop([`${ANSI_FG_YELLOW}%s${ANSI_FG_NC}`, config.common.messages.untrackedGitFiles]);
+exec('git branch -a', execAction((branches) => {
+  const isTargetBranchExists = branches
+    .trim()
+    .split(EMPTY_LINE)
+    .some((branch) => branch === config.git.pr.target);
+  if (!isTargetBranchExists) {
+    stop([`${ANSI_FG_RED}%s${ANSI_FG_NC}`, config.common.messages.unavailableGitTargetBranch]);
   }
-  exec(`npm version ${semver} -m "${semverMessage(semver)}" ${preidParam(preid)}`, execAction((_version) => {
-    const version = _version.trim(); // remove new line of the version number string
-    const versionBranchName = composeVersionBranchName(semver, version, branch);
-    exec(`git checkout -b ${versionBranchName}`, execAction(() => {
-      if (!push) {
-        Logger.stack([
-          [`${config.common.project.title} version updated %s`, `${ANSI_FG_GREEN}locally${ANSI_FG_NC}.`],
-          [`Version: %s`, `${ANSI_FG_GREEN}${version}${ANSI_FG_NC}`],
-          [`Semantic version: %s`, `${ANSI_FG_GREEN}${semver}${ANSI_FG_NC}${EMPTY_LINE}`],
-        ]);
-        stop();
-      }
-      exec(`git push ${config.git.remote} ${versionBranchName} --follow-tags`, execAction(() => {
-        const pullRequestUrl = generatePullRequestUrl(versionBranchName);
-        opn(pullRequestUrl);
-        Logger.stack([
-          [`${config.common.project.title} version updated %s`, `${ANSI_FG_GREEN}remotely${ANSI_FG_NC}.`],
-          [`Version: %s`, `${ANSI_FG_GREEN}${version}${ANSI_FG_NC}`],
-          [`Branch: %s`, `${ANSI_FG_GREEN}${versionBranchName}${ANSI_FG_NC}`],
-          [`Semantic version: %s`, `${ANSI_FG_GREEN}${semver}${ANSI_FG_NC}${EMPTY_LINE}`],
-          [`Pull request URL: %s`, `${ANSI_FG_GREEN}${pullRequestUrl}${ANSI_FG_NC}${EMPTY_LINE}`],
-        ]);
-        stop();
-      }, { error: true, stderr: false }))
-    }, { error: true }))
-  }))
+  exec('git status --porcelain', execAction((changes) => {
+    if (changes) {
+      stop([`${ANSI_FG_YELLOW}%s${ANSI_FG_NC}`, config.common.messages.untrackedGitFiles]);
+    }
+    exec(`npm version ${semver} -m "${semverMessage(semver)}" ${preidParam(preid)}`, execAction((_version) => {
+      const version = _version.trim(); // remove new line of the version number string
+      const versionBranchName = composeVersionBranchName(semver, version, branch);
+      exec(`git checkout -b ${versionBranchName}`, execAction(() => {
+        if (!push) {
+          Logger.stack([
+            [`${config.common.project.title} version updated %s`, `${ANSI_FG_GREEN}locally${ANSI_FG_NC}.`],
+            [`Version: %s`, `${ANSI_FG_GREEN}${version}${ANSI_FG_NC}`],
+            [`Semantic version: %s`, `${ANSI_FG_GREEN}${semver}${ANSI_FG_NC}${EMPTY_LINE}`],
+          ]);
+          stop();
+        }
+        exec(`git push ${config.git.remote} ${versionBranchName} --follow-tags`, execAction(() => {
+          const pullRequestUrl = generatePullRequestUrl(versionBranchName);
+          opn(pullRequestUrl);
+          Logger.stack([
+            [`${config.common.project.title} version updated %s`, `${ANSI_FG_GREEN}remotely${ANSI_FG_NC}.`],
+            [`Version: %s`, `${ANSI_FG_GREEN}${version}${ANSI_FG_NC}`],
+            [`Branch: %s`, `${ANSI_FG_GREEN}${versionBranchName}${ANSI_FG_NC}`],
+            [`Semantic version: %s`, `${ANSI_FG_GREEN}${semver}${ANSI_FG_NC}${EMPTY_LINE}`],
+            [`Pull request URL: %s`, `${ANSI_FG_GREEN}${pullRequestUrl}${ANSI_FG_NC}${EMPTY_LINE}`],
+          ]);
+          stop();
+        }, { error: true, stderr: false }));
+      }, { error: true }));
+    }));
+  }));
 }));
